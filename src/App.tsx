267b1279bbc926d69afda0376b5a0e1062f87664
jocalu <jocalu87@gmail.com>
2023-03-09 16:32:07 +0100

    Add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,46 @@
 import Title from './components/Title'
 import Footer from './components/Footer'
 import ListItem from './components/ListItem'
+import Button from './components/Button'
 import List from './components/List'
 import Card from './components/Card'
 import Header from './components/Header'
 import { users } from './db/users.json'
 
 interface User {
-  name: string
-  birthdayDate: string
-  image: string
+	name: string
+	birthdayDate: string
+	image: string
 }
 
 const App = () => {
-  return (
-    <div className=" w-full p-4 flex flex-col">
-      <Header>
-        <Title />
-      </Header>
-
-      <div className="flex flex-col items-center gap-y-4">
-        <Card title={'Birthdays'}>
-          <List>
-            {users.map((user: User) => (
-              <ListItem
-                key={user.birthdayDate}
-                name={user.name}
-                birthdayDate={user.birthdayDate}
-                image={user.image}
-              />
-            ))}
-          </List>
-        </Card>
-      </div>
-      <Footer text={'For friends and family only'} />
-    </div>
-  )
+	return (
+		<>
+			<Header>
+				<Title />
+			</Header>
+			<div className="w-full p-4 flex flex-col">
+				<div className="flex flex-col items-center gap-y-4">
+					<Card title={'Birthdays'}>
+						<List>
+							{users.map((user: User) => (
+								<ListItem
+									key={user.birthdayDate}
+									name={user.name}
+									birthdayDate={user.birthdayDate}
+									image={user.image}
+								/>
+							))}
+						</List>
+					</Card>
+				</div>
+				<div className="flex justify-center pt-10">
+					<Button label={'Add events to the calendar'} />
+				</div>
+				<Footer text={'For friends and family only'} />
+			</div>
+		</>
+	)
 }
 
 export default App
---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+interface Props {
+	label: string
+}
+
+const Button = ({ label }: Props) => {
+	return (
+		<button className="relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white ">
+			<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+				{label}
+			</span>
+		</button>
+	)
+}
+
+export default Button
