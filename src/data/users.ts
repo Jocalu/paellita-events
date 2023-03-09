6ec8571cf0dd2344b29dbac287626975305774d8
jocalu <jocalu87@gmail.com>
2023-03-09 19:13:16 +0100

    Update dates and images

--- src/data/users.ts
+++ src/data/users.ts
@@ -5,102 +5,102 @@ export const users: User[] = [
     name: 'Jose',
     birthdayDate: '1987-01-01',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/jose.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/jose.png'
   },
   {
     name: 'Mer',
     birthdayDate: '1989-11-12',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/mer.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/mer.png'
   },
   {
     name: 'Bernat',
     birthdayDate: '1990-01-11',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/bernat.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/bernat.png'
   },
   {
     name: 'Mónica',
     birthdayDate: '1994-08-20',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/monica.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/monica.png'
   },
   {
     name: 'Caso',
     birthdayDate: '1990-01-19',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/caso.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/caso.png'
   },
   {
     name: 'Claudia',
     birthdayDate: '1993-09-20',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/claudia.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/claudia.png'
   },
   {
     name: 'Pau',
-    birthdayDate: '1990-05-14',
+    birthdayDate: '1990-04-14',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/pau.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/pau.png'
   },
   {
     name: 'Mireia',
     birthdayDate: '1990-12-03',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/mireia.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/mireia.png'
   },
   {
     name: 'Peris',
     birthdayDate: '1990-05-08',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/peris.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/peris.png'
   },
   {
     name: 'Irene',
     birthdayDate: '1988-09-02',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/irene.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/irene.png'
   },
   {
     name: 'Carlos',
     birthdayDate: '1990-08-26',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/carlos.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/carlos.png'
   },
   {
     name: 'Maria',
-    birthdayDate: '2000-12-31',
+    birthdayDate: '2000-03-14',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/default.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/maria.png'
   },
   {
     name: 'Dani',
     birthdayDate: '1990-11-26',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/dani.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/dani.png'
   },
   {
     name: 'Katya',
     birthdayDate: '1998-07-12',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/katya.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/katya.png'
   },
   {
     name: 'Juan',
     birthdayDate: '1991-09-27',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/juan.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/juan.png'
   },
   {
     name: 'Suki',
-    birthdayDate: '2000-12-31',
+    birthdayDate: '2000-08-10',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/default.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/suki.png'
   },
   {
     name: 'Raúl',
     birthdayDate: '1990-01-20',
     image:
-      'https://github.com/Jocalu/paellita-events/blob/a4ad48364251330758f370dfee220c7ae9db4b77/src/assets/images/raul.png?raw=true'
+      'https://raw.githubusercontent.com/Jocalu/paellita-events/main/src/assets/images/raul.png'
   }
 ]
