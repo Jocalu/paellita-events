90646f84c4072eef1ff3e2e57cc437a9c42d5a43
jocalu <jocalu87@gmail.com>
2023-03-10 10:01:32 +0100

    Trying google calendar api

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,14 @@
 interface Props {
   label: string
+  onClick?: () => void
 }
 
-const Button = ({ label }: Props) => {
+const Button = ({ label, onClick }: Props) => {
   return (
-    <button className="relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white ">
+    <button
+      className="relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white"
+      onClick={onClick}
+    >
       <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
         {label}
       </span>
