bd1bf77b4281ce9d5a34dea3b986bee7c068a40c
jocalu <jocalu87@gmail.com>
2023-03-10 10:48:44 +0100

    Remove google calendar

--- src/App.tsx
+++ src/App.tsx
@@ -13,61 +13,6 @@ import { changeDateFormat, sortByNextBirthday } from './utils/dates'
 const transformedUsers = changeDateFormat(sortByNextBirthday(users))
 
 const App = () => {
-  const gapi = (window as any).gapi
-  const CLIENT_ID =
-    '736340294046-qf6c4u0s4s5obusr2udgbrsd4m83pho5.apps.googleusercontent.com'
-  const API_KEY = 'AIzaSyB3RaDKH6350LaEvgHNahNyrvhCs8JN6y0'
-  const DISCOVERY_DOCS = [
-    'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'
-  ]
-  const SCOPES = 'https://www.googleapis.com/auth/calendar.events'
-
-  const handleClick = () => {
-    gapi.load('client:auth2', () => {
-      gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-      })
-
-      gapi.client.load('calendar', 'v3')
-
-      gapi.auth2
-        .getAuthInstance()
-        .signIn()
-        .then(() => {
-          const event = {
-            summary: 'Awesome Event!',
-            description: 'Really great refreshments',
-            start: {
-              dateTime: '2023-06-28T09:00:00-07:00',
-              timeZone: 'Europe/Madrid'
-            },
-            end: {
-              dateTime: '2023-06-28T17:00:00-07:00',
-              timeZone: 'Europe/Madrid'
-            },
-            recurrence: ['RRULE:FREQ=YEARLY;COUNT=2'],
-            reminders: {
-              useDefault: false,
-              overrides: [{ method: 'popup', minutes: 10 }]
-            }
-          }
-
-          const request = gapi.client.calendar.events.insert({
-            calendarId: 'primary',
-            resource: event
-          })
-
-          request.execute((event: any) => {
-            console.log('event', event)
-            window.open(event.htmlLink)
-          })
-        })
-    })
-  }
-
   return (
     <>
       <Header>
@@ -88,8 +33,8 @@ const App = () => {
             </List>
           </Card>
         </div>
-        <div className="flex justify-center pt-10">
-          <Button label={'Add events to the calendar'} onClick={handleClick} />
+        <div className="hidden justify-center pt-10">
+          <Button label={'Add events to the calendar'} />
         </div>
       </div>
       <Footer text={'For friends and family only'} />
